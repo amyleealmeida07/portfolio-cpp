0edaa043963c5f069e6f3c2a794a29cc72d9d617
Amylee Almeida <amyleeagatha465@gmail.com>
2026-08-12 10:26:48 -0300

    Create metade_numero.cpp

---
+++ 01-basico-e-io/metade_numero.cpp
@@ -0,0 +1,16 @@
+// Lê um número e exibe a sua metade
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double numero;
+    
+    cout << "Informe um número: ";
+    cin >> numero;
+    
+    cout << "A metade do número informado é: " << numero / 2.0 << endl;
+    
+    return 0;
+}
